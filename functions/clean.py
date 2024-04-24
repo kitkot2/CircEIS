a14db16a33ed7720f6a67533344f55e497a08aa3
Никита Котенко <kotenko.n.a@yandex.ru>
2024-04-24 22:18:31 +0300

    added filters

--- functions/clean.py
+++ functions/clean.py
@@ -1,4 +1,6 @@
 from functions import show
+from scipy.signal import lfilter
+
 def simple(Re: list, Im : list):
     x, y=[], []
     ReM = 0
@@ -12,9 +14,22 @@ def simple(Re: list, Im : list):
     show.plot_data(x,y)
     return x, y
 
+# Not working
+def lfilter_scipy(x: list, y : list):
+    n = 10  # the larger n is, the smoother curve will be
+    b = [1.0 / n] * n
+    a = 1
+    yy = lfilter(b, a, y)
+    print('Cleaned_data:')
+    show.plot_data(x,yy)
+    return x, yy
+
 def clean_hub(Re: list, Im : list):
     print('To clean data enter 1, else enter 0')
-    if int(input()):
+    fl = int(input())
+    if fl == 1:
         return simple(Re, Im)
+    elif fl == 2:
+        return lfilter_scipy(Re, Im)
     else:
         return Re, Im
