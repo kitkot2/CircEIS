e64a11b5731ff8ba8e6401708eedf82c473b1eef
Никита Котенко <kotenko.n.a@yandex.ru>
2024-04-28 16:37:53 +0300

    circuit fitting update

--- circuit_fitting.py
+++ circuit_fitting.py
@@ -1,26 +1,24 @@
-from impedance.models.circuits import CustomCircuit
+import impedance.models.circuits as circuits
 import matplotlib.pyplot as plt
 from impedance.visualization import plot_nyquist
 
+#Work in progress
 def circuit_fit(frequencies, Z):
     
-    print('Enter your circuit: ')
-    circuit = str(input())
-    circuit = 'R0-p(R1,C1)-p(R2-Wo1,C2)'
+    print('Enter your circuit name: ')
+    circuit_name = str(input())
     
-    print('Enter your initial guess: ')
-    
-    #Work in progress
-    
-    initial_guess = [.01, .01, 100, .01, .05, 100, 1]
+    # loading from circuits
+    circuit = circuits.CustomCircuit()
+    circuit.load(f'circuits/{circuit_name}.json')
 
-    circuit = CustomCircuit(circuit, initial_guess=initial_guess)
-
+    # Fitting
     circuit.fit(frequencies, Z)
-
+    
+    # Fitted circuit info
     print(circuit)
 
+    
     circuit.plot(f_data=frequencies, Z_data=Z, kind='nyquist')
-
     plt.legend(['Data', 'Fit'])
     plt.show()
