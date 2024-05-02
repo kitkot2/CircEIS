f865ce58059941db042e2458e9d9868699fbdab8
Никита Котенко <kotenko.n.a@yandex.ru>
2024-05-02 14:21:48 +0300

    addded CPE alpha parameter calculation

--- functions/math.py
+++ functions/math.py
@@ -9,3 +9,20 @@ def intersections_with_ox(xc, yc, R):
         right_intersection_x = xc + math.sqrt(R**2 - (yc)**2)
         left_intersection_x = xc - math.sqrt(R**2 - (yc)**2)
         return left_intersection_x, right_intersection_x
+    
+def getAngle(a, b, c):
+    ang = math.degrees(math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
+    return ang + 360 if ang < 0 else ang
+
+def get_CPE_alpha(xc, yc, left_intersection):
+    angle = getAngle((xc, yc),(left_intersection, 0),(xc,0))
+    
+    # angle = 0 => alpha = 1
+    # angle = 45 => alpha = 0.5
+    # angle = 90 => alpha = 0
+    
+    if angle < 0 or angle > 90:
+        return None
+    else:
+        alpha = 1 - (angle/90)
+        return alpha
